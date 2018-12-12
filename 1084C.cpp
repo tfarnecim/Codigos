f80bb489186499aece1b29ec689a78a9f0dd92e7
tfarnecim <36956453+tfarnecim@users.noreply.github.com>
2018-12-12 14:07:17 -0200

    Add files via upload

---
+++ 1084C.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+int main(){
+	
+	string s;
+	
+	cin >> s;
+	
+	ll mult = 1;
+	ll at = 1;
+	
+	s += 'b';
+	
+	for(ll i=0;i<s.length();i++){
+		
+		if(s[i]=='b'){
+			
+			//mult *= at;
+			
+			mult = ( (mult%1000000007) * (at%1000000007) ) % 1000000007;
+			
+			at = 1;
+			
+		}else if(s[i]=='a'){
+			
+			at++;
+		
+		}
+		
+	}
+	
+	cout << mult - 1 << endl;
+	
+	return 0;
+}
